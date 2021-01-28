013b65419f1d29feb9e15568b9288dde83aa105e
whwogus947 <78160815+whwogus947@users.noreply.github.com>
2021-01-29 00:52:28 +0900

    TEST
    test01

---
+++ SpineTest/Assets/Scripts/BagManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagManager : MonoBehaviour
+{
+    public List<ItemObject> objectsInMyBag = new List<ItemObject>();
+    public static bool isOjectGripped;
+    public Collider col;
+
+    Ray2D ray;
+    RaycastHit2D rayHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOjectGripped = true;
+    }
+
+    void Update()
+    {
+        if (isOjectGripped)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                rayHit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, 1 << LayerMask.NameToLayer("UILayer"));
+                if (rayHit.collider == col)
+                {
+                    Debug.Log("obj");
+                }
+            }
+        }
+    }
+}
